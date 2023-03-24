d2494b058852ddef02bc6e67344aba07b3b04d55
LAPTOP-NAG7AC3E\admin <hieunguyenduyenhai22092002@gmail.com>
2023-03-24 16:19:01 +0700

    them file nha, tru, quai vat

---
+++ enemy.py
@@ -0,0 +1,25 @@
+# class Player:
+#     def __int__(self):
+#         self.mau = 1000
+#         self.tancong = 10
+#         self.thu = 30
+#         self.hitbox = (1, 1)
+#         self.vel = 10#van toc
+#         self.hinhanhTrai = [] #hinh anh de chuyen dong quai vat
+#         self.hinhanhPhai = []
+#         self.x = 100#toa do
+#         self.y = 100
+#         self.tonTai = True
+#
+#     def draw(self):
+#         #di chuyen ra xa nhan vat neu mau < 50%
+#         #ham de ve quai vat khi chuyen dong neu tonTai = True
+#
+#     def tanCong(self, loai):
+#         #ramdom de chon loai tan cong
+#
+#     def biDanhTrung(self, chiSo):
+#         self.mau -= chiSo
+#         #am thanh
+#         if self.mau <= 0:
+#             self.tonTai = False
